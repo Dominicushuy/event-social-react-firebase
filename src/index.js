8257407dc17592bc164a40a8d80219e20bb382af
Dominicushuy <huypro1996z@gmail.com>
2020-10-15 19:41:07 +0700

    test branch

--- src/index.js
+++ src/index.js
@@ -34,8 +34,4 @@ if (module.hot) {
 }
 
 render();
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.register();
